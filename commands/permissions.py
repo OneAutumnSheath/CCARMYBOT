9783979646833769214d4c7d04be674c199fc44a
OneAutumnSheath <info@oneautumnsheath.de>
2025-02-01 18:53:06 +0100

    edited permissions.py to load correctly

--- commands/permissions.py
+++ commands/permissions.py
@@ -123,4 +123,4 @@ async def delpermission(ctx, user: discord.User = None, role: discord.Role = Non
 
 # Setup-Funktion zum Hinzufügen des Cogs
 async def setup(bot):
-    await bot.add_cog(Permissions(bot))
+    await bot.add_cog(Permissions(bot))  # Hier wird der Cog korrekt hinzugefügt
