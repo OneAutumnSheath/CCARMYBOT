a8bbe1376d4cef5ffae47cd64f5ee677efbf3c28
OneAutumnSheath <info@oneautumnsheath.de>
2025-02-01 10:27:44 +0100

    added async and await in main.py; forgot that discord.py wants async in on_ready

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 @bot.event
-def on_ready():
+async def on_ready():
     print(f"Bot ist bereit! Eingeloggt als {bot.user} (ID: {bot.user.id})")
 
     for filename in os.listdir('./commands'):
         if filename.endswith('.py'):
-            bot.load_extension(f'commands.{filename[:-3]}')
+            await bot.load_extension(f'commands.{filename[:-3]}')
 
 
 token = os.getenv("DISCORD_TOKEN")
