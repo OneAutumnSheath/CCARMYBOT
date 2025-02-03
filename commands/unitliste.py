d670eda9dacd3a0877ae6e60598199894eeee1a7
OneAutumnSheath <info@oneautumnsheath.de>
2025-02-03 11:23:29 +0100

    🐛 FIX: send message

--- commands/unitliste.py
+++ commands/unitliste.py
@@ -57,6 +57,7 @@ class UnitManager(commands.Cog):
             await channel.send(f"📊 Mitglieder der Einheit {unit_name}:\n" + "\n".join(member_mentions))
 
 
+
     @app_commands.command(name="setchannel", description="Setzt die Channel-ID für eine Einheit.")
     async def setchannel(self, interaction: discord.Interaction, unit_name: str, channel: discord.TextChannel):
         """Setzt den Channel für eine Einheit."""
